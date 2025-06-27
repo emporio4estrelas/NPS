e6b8830f947b37c145b27e241eeab1d2e1eeb8d4
emporio4estrelas <vitor.padetti@emporioquatroestrelas.com.br>
2025-06-27 10:53:41 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,28 +3,36 @@ using PesquisaSatisfacaoApi.Data;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite("Data Source=pesquisa.db"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy  =>
-                      {
-                          policy.WithOrigins("https://pesquisa-satisfacao-site.s3.us-east-1.amazonaws.com")
-                                .AllowAnyHeader()
-                                .AllowAnyMethod();
-                      });
+        policy =>
+        {
+            policy.WithOrigins("https://pesquisa-satisfacao-site.s3.us-east-1.amazonaws.com")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
 });
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(MyAllowSpecificOrigins); // Deve vir antes do Authorization
+
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
