2ac47cf9c1a20cf697c48ec3b627a861d1368200
emporio4estrelas <vitor.padetti@emporioquatroestrelas.com.br>
2025-06-27 11:55:55 -0300

    Add files via upload

---
+++ Migrations/20250627145226_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PesquisaSatisfacaoApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Respostas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Q1 = table.Column<string>(type: "TEXT", nullable: true),
+                    Q2 = table.Column<string>(type: "TEXT", nullable: true),
+                    Comentario = table.Column<string>(type: "TEXT", nullable: true),
+                    Pdv = table.Column<string>(type: "TEXT", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Respostas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Respostas");
+        }
+    }
+}
