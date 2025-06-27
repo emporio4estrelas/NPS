8e3633ae0e68e6da84f5c9705f80de0d03fe21b1
emporio4estrelas <vitor.padetti@emporioquatroestrelas.com.br>
2025-06-27 13:23:04 -0300

    Add files via upload

---
+++ Migrations/20250627162122_PostgreSqlCreate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PesquisaSatisfacaoApi.Migrations
+{
+    public partial class PostgreSqlCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
