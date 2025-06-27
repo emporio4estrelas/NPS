4d79ddceddd6f2387ee800aa1f5430ed88fc9f8b
emporio4estrelas <vitor.padetti@emporioquatroestrelas.com.br>
2025-06-27 10:36:05 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PesquisaSatisfacaoApi.Data;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
@@ -8,10 +9,22 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("https://pesquisa-satisfacao-site.s3.us-east-1.amazonaws.com")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
