fcd954e9664b43812dd5601f3ffc6a6c7d24764d
emporio4estrelas <vitor.padetti@emporioquatroestrelas.com.br>
2025-06-27 11:55:33 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -35,4 +35,11 @@ app.UseCors(MyAllowSpecificOrigins); // Deve vir antes do Authorization
 app.UseAuthorization();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+
 app.Run();
